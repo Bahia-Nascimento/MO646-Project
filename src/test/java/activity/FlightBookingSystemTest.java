5e05665b950b4b4d6290ac83f1c349ec79f728d6
Gabriel Nascimento <g222103@dac.unicamp.br>
2024-10-10 19:19:16 -0300

    flightBookingSystem tests 100% mutation coverage

--- src/test/java/activity/FlightBookingSystemTest.java
+++ src/test/java/activity/FlightBookingSystemTest.java
@@ -147,7 +147,86 @@ public class FlightBookingSystemTest {
         assertFalse(result.pointsUsed);
         assertEquals(0, result.refundAmount, 0.01);
     }
+
+    @Test
+    public void testEnoughSeatsBoundary() {
+        flight.passengers = 2;
+        flight.bookingTime = LocalDateTime.now();
+        flight.departureTime = LocalDateTime.now().plusHours(50);
+        flight.availableSeats = 2;
+        flight.currentPrice = 500;
+        flight.previousSales = 10;
+        flight.isCancellation = false;
+        flight.rewardPointsAvailable = 0;
     
+        FlightBookingSystem.BookingResult result = flightBookingSystem.bookFlight(flight.passengers, flight.bookingTime, flight.availableSeats,
+        flight.currentPrice, flight.previousSales, flight.isCancellation, flight.departureTime, flight.rewardPointsAvailable);
+    
+        assertTrue(result.confirmation);
+        assertEquals(80, result.totalPrice, 0.01);
+        assertFalse(result.pointsUsed);
+        assertEquals(0, result.refundAmount, 0.01);
+    }    
+
+    @Test
+    public void testLastMinuteBoundary() {
+        flight.passengers = 2;
+        flight.bookingTime = LocalDateTime.now();
+        flight.departureTime = flight.bookingTime.plusHours(24);
+        flight.availableSeats = 10;
+        flight.currentPrice = 500;
+        flight.previousSales = 10;
+        flight.isCancellation = false;
+        flight.rewardPointsAvailable = 0;
+    
+        FlightBookingSystem.BookingResult result = flightBookingSystem.bookFlight(flight.passengers, flight.bookingTime, flight.availableSeats,
+        flight.currentPrice, flight.previousSales, flight.isCancellation, flight.departureTime, flight.rewardPointsAvailable);
+    
+        assertTrue(result.confirmation);
+        assertEquals(80, result.totalPrice, 0.01);
+        assertFalse(result.pointsUsed);
+        assertEquals(0, result.refundAmount, 0.01);
+    }
+
+    @Test
+    public void testGroupDiscountBoundary() {
+        flight.passengers = 4;
+        flight.bookingTime = LocalDateTime.now();
+        flight.departureTime = LocalDateTime.now().plusHours(50);
+        flight.availableSeats = 10;
+        flight.currentPrice = 500;
+        flight.previousSales = 10;
+        flight.isCancellation = false;
+        flight.rewardPointsAvailable = 0;
+    
+        FlightBookingSystem.BookingResult result = flightBookingSystem.bookFlight(flight.passengers, flight.bookingTime, flight.availableSeats,
+        flight.currentPrice, flight.previousSales, flight.isCancellation, flight.departureTime, flight.rewardPointsAvailable);
+    
+        assertTrue(result.confirmation);
+        assertEquals(160, result.totalPrice, 0.01);
+        assertFalse(result.pointsUsed);
+        assertEquals(0, result.refundAmount, 0.01);
+    }
+
+    @Test
+    public void testFullRefundBoundaryCase() {
+        flight.passengers = 2;
+        flight.bookingTime = LocalDateTime.now();
+        flight.departureTime = LocalDateTime.now().plusHours(48);
+        flight.availableSeats = 10;
+        flight.currentPrice = 500;
+        flight.previousSales = 10;
+        flight.isCancellation = true;
+        flight.rewardPointsAvailable = 0;
+    
+        FlightBookingSystem.BookingResult result = flightBookingSystem.bookFlight(flight.passengers, flight.bookingTime, flight.availableSeats,
+        flight.currentPrice, flight.previousSales, flight.isCancellation, flight.departureTime, flight.rewardPointsAvailable);
+    
+        assertFalse(result.confirmation);
+        assertEquals(0, result.totalPrice, 0.01);
+        assertFalse(result.pointsUsed);
+        assertEquals(80, result.refundAmount, 0.01);
+    }    
 
     // @Test
     // public void testWeirdRefundInteraction() {
